96a54863e4aef5d47db8a1ba2acfbaa0502a95da
nickpoerio <37049226+nickpoerio@users.noreply.github.com>
2018-06-16 22:43:38 +0200

    t1b

--- src/main.cpp
+++ src/main.cpp
@@ -50,7 +50,7 @@ int main()
   PID pid2;
   pid2.Init(.1,.001,0.);
 
-  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
+  h.onMessage([&pid,&pid2](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event

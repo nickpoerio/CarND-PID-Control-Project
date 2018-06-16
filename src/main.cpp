847fdae90e1c3cb3546f9e99a372fbce24bc306e
nickpoerio <37049226+nickpoerio@users.noreply.github.com>
2018-06-16 23:11:19 +0200

    t10

--- src/main.cpp
+++ src/main.cpp
@@ -45,10 +45,10 @@ int main()
   //pid.Init(.2,.0001,4); the best so far, still nervous
   //pid.Init(.15,.0001,2.5); 
   //pid.Init(.13,.0,2.6);
-  pid.Init(.03,.0,.6);
+  pid.Init(.04,.0,.8);
   
   PID pid2;
-  pid2.Init(.1,.00001,0.);
+  pid2.Init(.1,.00005,0.);
 
   h.onMessage([&pid,&pid2](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.

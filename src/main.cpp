ce7477c1668ae9b0ee2fbcaf6ec9b6c2256284bb
nickpoerio <37049226+nickpoerio@users.noreply.github.com>
2018-06-16 23:58:03 +0200

    t27

--- src/main.cpp
+++ src/main.cpp
@@ -45,7 +45,7 @@ int main()
   //pid.Init(.2,.0001,4); the best so far, still nervous
   //pid.Init(.15,.0001,2.5); 
   //pid.Init(.13,.0,2.6);
-  pid.Init(.05,.0,1.8);
+  pid.Init(.055,.0,2.2);
   
   PID pid2;
   pid2.Init(.3,.00003,0.);
